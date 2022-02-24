607541adad8fb8b8c06d7f063e181cbc4ecf61b9
nadezdatsygankova <nadiapritchina@gamil.com>
2022-02-23 22:39:17 -0500

    add AuthContext

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,38 @@
 import React from "react";
 import { ThemeContext } from '../contexts/ThemeContext';
+import { AuthContext } from '../contexts/AuthContext';
 class Navbar extends React.Component {
     render() {
         return (
-            <ThemeContext.Consumer>{(context) => {
-                const { isDarkTheme, darkTheme, lightTheme } = context;
-                const theme = isDarkTheme ? darkTheme : lightTheme;
+            <AuthContext.Consumer>{(authContext) => {
                 return (
-                    <nav style={{ background: theme.background, color: theme.text, height: '120px' }}>
-                        <h2 className="ui centered header">
-                            Oak Academy
-                        </h2>
-                        <div className="ui three buttons">
-                            <button className="ui button">Overview</button>
-                            <button className="ui button">Contact</button>
-                            <button className="ui button">Support</button>
-                        </div>
-                    </nav>
+                    <ThemeContext.Consumer>{(themeContext) => {
+                        const { isDarkTheme, darkTheme, lightTheme } = themeContext;
+                        const theme = isDarkTheme ? darkTheme : lightTheme;
+                        const { isLoggedIn, changeAuthStatus } = authContext;
+                        return (
+                            <nav style={{ background: theme.background, color: theme.text, height: '120px' }}>
+                                <h2 className="ui centered header">
+                                    Oak Academy
+                                </h2>
+                                <p
+                                    onClick={changeAuthStatus}
+                                    style={{ textAlign: 'center' }}
+                                >
+                                    {isLoggedIn ? 'logged in' : 'logged out'}
+                                </p>
+                                <div className="ui three buttons">
+                                    <button className="ui button">Overview</button>
+                                    <button className="ui button">Contact</button>
+                                    <button className="ui button">Support</button>
+                                </div>
+                            </nav>
+                        )
+                    }}
+                    </ThemeContext.Consumer>
                 )
             }}
-            </ThemeContext.Consumer>
+            </AuthContext.Consumer>
         );
     }
 }
